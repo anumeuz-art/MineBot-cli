5c19c3799f7e5ccbbdae2ae2a4ab782a9baf4e7c
anumeuz-art <anumeuz@gmail.com>
2026-04-20 16:03:59 +0500

    Update channel names and remove logo

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ ADMIN_IDS = [5703605946] # Твой ID администратора
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 GEMINI_KEY = os.getenv("GEMINI_KEY")
 
-CHANNELS_STR = os.getenv("CHANNELS", "@lazikosmods")
+CHANNELS_STR = os.getenv("CHANNELS", "@lazikomods")
 AVAILABLE_CHANNELS = [ch.strip() for ch in CHANNELS_STR.split(',')]
 DEFAULT_CHANNEL = AVAILABLE_CHANNELS[0] if AVAILABLE_CHANNELS else ""
 
-WATERMARK_TEXT = "@lazikosmods"
+WATERMARK_TEXT = "@lazikomods"
 
 # 🧠 НОВАЯ НАСТРОЙКА: Интервал умной очереди (в часах)
 SMART_QUEUE_INTERVAL_HOURS = 8
